7f9dc9123be00dc3ff9a0cd09ac6e2391b1273f5
Neo-Leo <sharm398@umn.edu>
2016-10-25 01:18:36 -0500

    Add files via upload

---
+++ Duplicates.java
@@ -0,0 +1,30 @@
+/*
+ * Nilanshu Sharma
+ * https://leetcode.com/problems/find-all-duplicates-in-an-array/
+ */ 
+
+public class Solution {
+    
+    public List<Integer> findDuplicates(int[] nums) {
+        
+        List<Integer> resList = new ArrayList<Integer>(); 
+        
+        for(int elem : nums) {
+            
+            if(nums[Math.abs(elem)-1] < 0) {
+                
+                resList.add(Math.abs(elem)); 
+            
+            } else {
+                
+                nums[Math.abs(elem)-1] *= -1; 
+            
+            }
+        
+        }
+        
+        return resList; 
+    
+    }
+
+}
